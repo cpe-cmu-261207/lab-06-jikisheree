19b57cd9370a500fdde8374f772b0afff8985a91
Suparida <jik2002x@gmail.com>
2021-08-29 21:54:44 +0700

    ver1 finished

---
+++ src/components/About.tsx
@@ -0,0 +1,11 @@
+
+const About = () => {
+    return (
+        <div className='text-center space-y-3'>
+            <p className='text-2xl font-semibold'>About me</p>
+            <p className='text-xl'>Suparida Silpasith 630610765</p>
+        </div>
+    )
+}
+
+export default About
